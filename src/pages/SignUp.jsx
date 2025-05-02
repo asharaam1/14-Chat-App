a15acd2a962910667a3656227aa44b8b72eac9d9
asharaam1 <lohanaasharaam1@gmail.com>
2025-05-02 20:09:12 +0500

    SignUp Auth-Firebase

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button, Container, Typography, Box } from '@mui/material';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firbaseconfig';
+import Swal from 'sweetalert2';
 
 const SignUp = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+
+
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password, name)
+            setName('');
+            setEmail('');
+            setPassword('');
+
+            //! Sweet Alert 
+            Swal.fire({
+                title: "SignUp Success",
+                icon: "success",
+                draggable: true
+            });
+            
+        } catch (error) {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            Swal.fire({
+                icon: "error",
+                title: errorCode,
+            });
+        }
+    }
+
+
+
+
     return (
         <Container maxWidth="xs">
             <Box
@@ -21,6 +58,8 @@ const SignUp = () => {
                     fullWidth
                     margin="normal"
                     type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                 />
 
                 <TextField
@@ -29,6 +68,8 @@ const SignUp = () => {
                     fullWidth
                     margin="normal"
                     type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <TextField
@@ -37,14 +78,8 @@ const SignUp = () => {
                     fullWidth
                     margin="normal"
                     type="password"
-                />
-
-                <TextField
-                    label="Confirm Password"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
 
                 <Button
@@ -52,6 +87,7 @@ const SignUp = () => {
                     color="primary"
                     fullWidth
                     sx={{ mt: 2 }}
+                    onClick={handleSignUp}
                 >
                     Sign Up
                 </Button>
